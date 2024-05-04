0ffcfdacdf853172580a1670155803f2bf356a6e
Gourav Gurjar <gouravgurjarnishali143@gmail.com>
2024-05-04 17:39:26 +0530

    added new code

---
+++ com/gg/arrays/testdemo.java
@@ -0,0 +1,17 @@
+package com.gg.arrays;
+
+public class testdemo {
+	
+	public static void main(String[] args) {
+		
+		int [] a = {100,200,400,500,700};
+		
+		for(int i= 0;i<a.length;i++) {
+			
+			
+			System.out.println(a[i]);
+			
+		}
+	}
+
+}
