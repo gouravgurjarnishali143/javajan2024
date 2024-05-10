13b3223525fb019a2b3d0954f39b10ff7b7ce74d
Gourav Gurjar <gouravgurjarnishali143@gmail.com>
2024-05-10 10:04:35 +0530

    added new code

---
+++ com/gg/opps/ResolutionClient.java
@@ -0,0 +1,16 @@
+package com.gg.opps;
+
+public class ResolutionClient {
+	public static void main(String[] args) {
+		
+		ResolutionDemo resolutionDemo = new ResolutionDemo();
+		
+	     resolutionDemo.m1(new Object());
+	     
+	     resolutionDemo.m1("Amit");
+	     
+	     resolutionDemo.m1(null);
+	}
+	
+
+}
---
+++ com/gg/opps/ResolutionDemo.java
@@ -0,0 +1,22 @@
+package com.gg.opps;
+
+public class ResolutionDemo {
+	
+	public void m1(Object obj) {
+		
+		
+		System.out.println("object version");
+		
+		
+		
+		}
+	public void m1(String s) {
+		
+		
+		System.out.println("String version");
+		
+	}
+	}
+	
+
+
