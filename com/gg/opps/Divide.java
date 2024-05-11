88b3294f7b198fb6191e4ddc827840efcfc26f89
Gourav Gurjar <gouravgurjarnishali143@gmail.com>
2024-05-11 12:35:38 +0530

    new code added

---
+++ com/gg/opps/Divide.java
@@ -0,0 +1,21 @@
+package com.gg.opps;
+
+public class Divide {
+	
+	public int Divide(int x,int y ) {
+		
+		return x / y;
+	
+	}
+	
+	public int Divide(int x,int y,int z) {
+		
+		return x/y/z;
+	}
+	
+	public double Divide(double x ,double y) {
+		
+		return x/y;
+	}
+
+}
